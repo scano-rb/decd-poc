c2772589fae2eaf7ecea38fa882b3b57d28ca3f2
Sergio Cano <sergio.cano@redb.ee>
2021-03-06 11:47:43 -0300

    healthcheck unit test

--- build.sbt
+++ build.sbt
@@ -7,11 +7,8 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.13.3"
 
-libraryDependencies += guice
-libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
-
-// Adds additional packages into Twirl
-//TwirlKeys.templateImports += "com.decd.controllers._"
-
-// Adds additional packages into conf/routes
-// play.sbt.routes.RoutesKeys.routesImport += "com.decd.binders._"
+libraryDependencies ++= Seq(
+  guice,
+  // test
+  "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+)
---
+++ test/controllers/HealthCheckControllerSpec.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import org.scalatest.BeforeAndAfter
+import org.scalatestplus.play.PlaySpec
+import play.api.test.Helpers._
+import play.api.test._
+
+class HealthCheckControllerSpec extends PlaySpec with BeforeAndAfter {
+
+  private val subject = new HealthCheckController(Helpers.stubControllerComponents())
+
+  "HealthCheckController" should {
+    "return healthcheck response" in {
+
+      val result = subject.healthCheck().apply(FakeRequest())
+
+      contentAsString(result) mustBe "ok"
+      contentType(result) mustBe Some("text/plain")
+      status(result) mustBe 200
+    }
+  }
+}
