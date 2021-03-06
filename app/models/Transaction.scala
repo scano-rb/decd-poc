9d957fad4fe7cedc9b686b8c654a75d9667ac65a
Sergio Cano <68248660+scano-rb@users.noreply.github.com>
2021-03-06 13:12:29 -0300

    domain (#5)

---
+++ app/models/Transaction.scala
@@ -0,0 +1,12 @@
+package models
+
+case class Transaction(
+  id: Option[Long],
+  amount: BigDecimal,
+  cardLastDigits: String,
+  dateTime: String,
+  installments: Int,
+  cardType: String,
+  userId: Long,
+  status: Option[String]
+)
