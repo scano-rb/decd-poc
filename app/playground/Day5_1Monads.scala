4f965e62d8436eed7b8b2ff546aece5a5b9183c7
Sergio Cano <sergio.cano@redb.ee>
2021-03-13 19:02:57 -0300

    day 5 - monads and Future intro

---
+++ app/playground/Day5_1Monads.scala
@@ -0,0 +1,64 @@
+package playground
+
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+object Day5_1Monads extends App {
+
+  // Una mónada es un tipo de dato genérico, que implementa los siguientes métodos
+  trait Monad[T] {
+
+    def pure[T](t: T): Monad[T]
+
+    def flatMap[B](f: T => Monad[B]): Monad[B]
+  }
+
+  // cualquier cosa que tenga pure y flatmap es una monada
+
+  // la monad es un patron que permite secuenciar computaciones
+
+  // Ejemplo 1 - Option
+
+  // en el caso de Option, esta monada representa un valor que puede estar presente o no
+  // No obstante, al ser una monada, nos permite poder encadenar diferentes computaciones
+  val option = Option(1)
+    .flatMap(value => Option(value * 2))
+    .flatMap(value => Option(value * 2))
+    .flatMap(value => Option(value * 2))
+    .flatMap(value => Option(value * 2))
+    .flatMap(value => Option(value * 2))
+
+  // Ejemplo 2 - Option: imaginemos que tenemos una case class User y los siguientes métodos de negocio
+  case class User(id: Long, username: String, email: String, password: String)
+
+  def validateParam(id: Long): Option[String] =
+    if (id > 0) Some("ok") else None
+
+  def getUserFromDB(id: Long): Option[User] = Some(User(3, "pepe", "pepe@pepe.com", "pass"))
+
+  def encryptData(password: String): Option[String] = Some(password.toUpperCase)
+
+  val userId = 3
+
+  /*
+    Si quisieramos hacer las siguientes operaciones en secuencia:
+
+      1. validar
+      2. obtener el usuario de la DB
+      3. encryptar su password
+
+      Podriamos hacerlo de la siguiente manera usando monadas
+   */
+
+  val result = validateParam(userId) //
+    .flatMap(_ => getUserFromDB(userId)) // Option[User]
+    .flatMap(user => encryptData(user.password)) // Option[String]
+
+  result match {
+    case Some(encryptedPass) =>
+      println(s"password enrypted: $encryptedPass")
+    case None =>
+      println("no se ha podido encriptar")
+  }
+}
---
+++ app/playground/Day5_2Futures.scala
@@ -0,0 +1,52 @@
+package playground
+
+import playground.Day5_1Monads.{User, userId}
+
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+object Day5_2Futures {
+  // Ejemplo 3 - ahora con Futures
+
+  // veamos el mismo ejemplo de Day5_1Monads pero con Futures
+
+  // nuestros métodos originales ahora van a ser todos asíncronos:
+
+  def validateParamAsync(id: Long): Future[String] =
+    if (id > 0) Future.successful("ok") else Future.failed(throw new IllegalArgumentException)
+
+  def getUserFromDBAsync(id: Long): Future[User] = Future.successful(User(3, "pepe", "pepe@pepe.com", "pass"))
+
+  def encryptDataAsync(password: String): Future[String] = Future.successful(password.toUpperCase)
+
+  // y queremos hacer las mismas operaciones en secuencia (validarParam --> getUserFromDB --> encryptData)
+
+  // la respuesta... mónadas de nuevo, pero esta vez usando Future
+
+  // instanciamos nuestro pool de threads
+  implicit val executionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(7))
+
+  // aca solamente se instanció asi para mostrar que el executionContext es un pool the threads
+  // (tranquilamente pueden importar scala.concurrent.ExecutionContext.Implicits.global y está bien)
+
+  // de esta forma encadenaríamos todas estas operaciones usando Future.
+  val resultAsync = validateParamAsync(userId) // hilo 3 ---> 3 ns
+    .flatMap(_ => getUserFromDBAsync(userId)) // hilo 2 --> 6 ns
+    .flatMap(user => encryptDataAsync(user.password)) // hilo 7
+
+  // forma de pattern matchear sobre el resultado/valor final (success o failure) del future
+  resultAsync.onComplete {
+    case Success(value) => println(s"el future retornó el valor. El valor es: $value")
+    case Failure(error) => println(s"ha ocurrido una exception mientras se ejecutaba el future: $error")
+  }
+
+  /*
+    Ver que hay dos tipos de escenarios al ejecutar Future:
+
+    1. la computación asíncrona se ejecutó correctamente y el Future te devuelve el valor/resultado de dicha operación
+    2. Ocurrió un error mientras se ejecutaba el Future (se disparó alguna excepción durante su ejecución, ejemplo:
+      querían hacer una query a la base y se cayó la misma, entonces les disparó una JDBC Exception y eso hizo que Future
+      fallara.)
+ */
+}
